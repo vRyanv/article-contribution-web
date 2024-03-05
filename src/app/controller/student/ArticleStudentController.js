4b2f6462ca11bf8e61987159e0f3d04da4bab3bb
Ryan <89388361+vRyanv@users.noreply.github.com>
2024-03-05 20:00:15 +0700

    done article management of coordinator and student

--- src/app/controller/student/ArticleStudentController.js
+++ src/app/controller/student/ArticleStudentController.js
@@ -1,4 +1,4 @@
-const {ArticleService, CommentService} = require("../../service");
+const {ArticleService, CommentService, MagazineService} = require("../../service");
 const {StatusCode, MagazineStatus} = require("../../constant");
 
 const ArticleStudentController = {
@@ -48,15 +48,42 @@ const ArticleStudentController = {
             role
         }
         return res.render(
-            'student/article/detail-article',
+            'student/article/detail/detail-article',
             {
                 layout: 'layout/portal/portal',
                 data
             }
         )
     },
-    ArticleContributionPage(req, res) {
+    async ArticleContributionPage(req, res) {
+        const article_list = await ArticleService.GetArticleListForStudent(req)
+        const magazine_list = await MagazineService.GetMagazineList()
+        article_list.forEach(article => {
+            const corresponding_magazine = magazine_list.find(
+                magazine => magazine._id.toString() === article.magazine.toString()
+            );
+            if (corresponding_magazine) {
+                if (!corresponding_magazine.articles) {
+                    corresponding_magazine.articles = [];
+                }
+                corresponding_magazine.articles.push(article);
+            }
+        });
 
+
+        const role = req.user.role
+        const data = {
+            page_title: 'Article',
+            magazine_list,
+            role
+        }
+        return res.render(
+            'student/article/management/article-management',
+            {
+                layout: 'layout/portal/portal',
+                data
+            }
+        )
     },
     async Create(req, res) {
         const article = await ArticleService.Create(req)
